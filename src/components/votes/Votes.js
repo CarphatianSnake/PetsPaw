c0a56c4b608f72db51e20e2c59106942bb211373
Dmytro <sunsn4ke@gmail.com>
2022-07-09 16:46:01 +0300

    feat: finish voting layout

--- src/components/votes/Votes.js
+++ src/components/votes/Votes.js
@@ -7,11 +7,36 @@ import './votes.scss';
 
 const Votes = () => {
   const url = cat;
+  const fakeLog = [
+    {id: '1kjhjk1', action: 'Likes', time: '22:47'},
+    {id: '2dfsfs', action: 'Dislikes', time: '22:22'},
+    {id: '3feffd', action: 'Favourites', time: '21:15'},
+    {id: '32rewfds', action: 'unfav', time: '19:16'}
+  ];
+
+  const logElements = fakeLog.map(el => {
+    const {id, action, time} = el;
+
+    const text = action === 'unfav' ? 'was removed from Favourites' : `was added to ${action}`;
+
+    return (
+      <li key={id} className='log-item'>
+        <div className='log-message'>
+          <span className="log-time">{time}</span>
+          <span className='log-text'>Image ID: <span className='log-photo-id'>{id}</span> {text}</span>
+        </div>
+        <div className={`log-action log-action-${action.toLowerCase()}-pic`}></div>
+      </li>
+    );
+    
+  });
+  
   return (
     <main>
       <SearchPanel />
       <section>
         <PageNavigation name="Voting" />
+
         <div style={{background: `no-repeat 50% url('${url}')`}} className="photo-container">
           <div className="votes-btns">
             <button className='vote-btn like'></button>
@@ -19,6 +44,10 @@ const Votes = () => {
             <button className='vote-btn dislike'></button>
           </div>
         </div>
+
+        <ul className='action-logs'>
+          {logElements}
+        </ul>
       </section>
     </main>
   );
