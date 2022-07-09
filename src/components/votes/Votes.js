c1e88b9703bbccd370352fab7b955a6f04f8d171
Dmytro <sunsn4ke@gmail.com>
2022-07-09 14:19:41 +0300

    feat: add voting photo container with buttons

--- src/components/votes/Votes.js
+++ src/components/votes/Votes.js
@@ -1,14 +1,24 @@
 import SearchPanel from '../searchPanel/SearchPanel';
 import PageNavigation from '../pageNavigation/PageNavigation';
 
+import cat from './img/cat.jpg';
+
 import './votes.scss';
 
 const Votes = () => {
+  const url = cat;
   return (
     <main>
       <SearchPanel />
       <section>
         <PageNavigation name="Voting" />
+        <div style={{background: `no-repeat 50% url('${url}')`}} className="photo-container">
+          <div className="votes-btns">
+            <button className='vote-btn like'></button>
+            <button className='vote-btn fav'></button>
+            <button className='vote-btn dislike'></button>
+          </div>
+        </div>
       </section>
     </main>
   );
