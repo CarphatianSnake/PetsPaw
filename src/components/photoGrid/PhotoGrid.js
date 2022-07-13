4f142383860003aba59f2470f57d97ac3e33c697
unknown <sunsn4ke@gmail.com>
2022-07-13 22:21:46 +0300

    feat: add hover effect with buttons to grid element at Breeds

--- src/components/photoGrid/PhotoGrid.js
+++ src/components/photoGrid/PhotoGrid.js
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 import GridNavBtns from './GridNavBtns'
 
@@ -8,25 +11,54 @@ const PhotoGrid = (props) => {
   
   const {name, photos} = props
 
+  const [onElement, setOnElement] = useState()
+  const navigate = useNavigate()
+
   const page = useSelector(state => state.pageSlice.gridPage)
 
-  const breedGrid = (page = 0) => {
+  const onGridOn = (e) => {
+    setOnElement(e.target.querySelector('div'))
+    e.target.querySelector('div').classList.add('grid-active')
+  }
+
+  const onGridOut = (e) => {
+    onElement.classList.remove('grid-active')
+  }
+
+  const breedElement = (id, name) => {
+
+    const onBreed = () => {
+      navigate(`../${id}`)
+    }
+
+    return (
+      <div className='breed-hover-element'>
+        <button onClick={onBreed} className='breed-btn'>{name}</button>
+      </div>
+    )
+  }
+
+  const grid = (page) => {
 
     return (
       <>
         <div className="photo-grid">
-          {photos[page].map(item => (
+          {photos[page].map(item => {
+            const element = breedElement(item.id, item.name)
+            return (
             item.photo ? 
-              <div key={item.id} style={{backgroundImage: `url('${item.photo.url}')`}} className='photo-grid-item'></div> :
-              <div key={item.id} className='photo-grid-item no-photo'></div>
-                ))}
+              <div onMouseEnter={(e) => onGridOn(e)} onMouseLeave={(e) => onGridOut(e)} key={item.id} style={{backgroundImage: `url('${item.photo.url}')`}} className='photo-grid-item'>{element}</div> :
+              <div onMouseEnter={(e) => onGridOn(e)} onMouseLeave={(e) => onGridOut(e)} key={item.id} className='photo-grid-item no-photo'>{element}</div>
+                )
+          })}
         </div>
         <GridNavBtns page={page} totalpages={photos.length - 1} />
       </>
     )
   }
 
-  return breedGrid(page)
+  return grid(page)
+
 }
 
 export default PhotoGrid;
